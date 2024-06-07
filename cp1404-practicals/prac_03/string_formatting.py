9ea57878a90d96eaf658c5e6a9441d9755ba0eb6
hiayethta <tajonera1643@gmail.com>
2024-06-07 11:03:49 +1000

    Add for loop to produce desired output

--- cp1404-practicals/prac_03/string_formatting.py
+++ cp1404-practicals/prac_03/string_formatting.py
@@ -49,3 +49,6 @@ print(f"{year} {name[:6]}{name[6:]} for about ${cost:,.0f}")
 # 2 ^ 8 is  256
 # 2 ^ 9 is  512
 # 2 ^10 is 1024
+for i in range(11):
+    result = 2 ** i
+    print(f"2 ^ {i} is {result:>4}")  # unsure of how to align last line
